ca9ebe280205246b712878dad408ceb95d388cea
LazarosZiskos <ziskoslaz@gmail.com>
2024-04-28 19:38:49 +0300

    nav finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,59 @@
 import { navLinks } from "@/constants";
 import Link from "next/link";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <header className="h-[30px] mt-5 mb-10">
+      <div className="container flex items-center justify-between">
+        <div className="text-xl font-bold md:text-3xl">
+          <a href="/">LINA AKAKIADOU</a>
+        </div>
+        <div className="md:flex space-x-10 hidden">
+          {navLinks.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className="font-semibold uppercase transition-all ease-in-out duration-200 hover:text-blue1 "
+            >
+              <h2>{link.title}</h2>
+            </Link>
+          ))}
+        </div>
+        <div className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <MenuIcon />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {navLinks.map((link, index) => (
+                <DropdownMenuItem
+                  key={index}
+                  className="pb-5 items-center justify-center"
+                >
+                  <Link
+                    href={link.href}
+                    className="font-semibold uppercase transition-all ease-in-out duration-100 hover:text-blue1 "
+                  >
+                    <h2>{link.title}</h2>
+                  </Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
